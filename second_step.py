fe04d3bce796767354b73b86663ce7c1cc0c2734
Leticia-SS <leticia.saraiva013@gmail.com>
2025-08-24 23:17:49 -0300

    Hashtable method

---
+++ second_step.py
@@ -0,0 +1,44 @@
+import sys
+import time
+
+def ler_arquivo_txt(nome_arquivo):
+    strings = []
+    try:
+        with open(nome_arquivo, 'r') as arquivo:
+            for linha in arquivo:
+                strings.append(linha.strip())
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+
+    return strings
+
+nome_arquivo = "output.txt"
+lista = ler_arquivo_txt(nome_arquivo)
+
+posicoes = [0, 99, 999, 4999, len(lista)-1]
+
+inicio_criacao = time.time()
+hashtable = {}
+for i, arquivo in enumerate(lista):
+    hashtable[i] = arquivo
+fim_criacao = time.time()
+
+memoria_ht = sys.getsizeof(hashtable)
+for key, value in hashtable.items():
+    memoria_ht += sys.getsizeof(key) + sys.getsizeof(value)
+
+inicio_recuperacao = time.time()
+arquivos_ht = {}
+for pos in posicoes:
+    arquivos_ht[pos] = hashtable.get(pos)
+fim_recuperacao = time.time()
+
+print("====================")
+print("Hashtable")
+print(f"Tempo de criação: {fim_criacao - inicio_criacao:.6f} segundos")
+print(f"Tempo de recuperação: {fim_recuperacao - inicio_recuperacao:.6f} segundos")
+print(f"Memória utilizada: {memoria_ht} bytes")
+print("\nArquivos recuperados:")
+for pos in posicoes:
+    print(f"Posição {pos + 1}: {hashtable.get(pos)}")
+print("\n")
