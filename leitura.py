ba044dd0ac01363398d79a9a5cd94ecac257591a
Leticia-SS <leticia.saraiva013@gmail.com>
2025-08-25 00:11:22 -0300

    Initial commit

---
+++ leitura.py
@@ -0,0 +1,20 @@
+def imprimir_string(strings):
+    for string in strings:
+        print(string)
+
+def let_arquivo_txt(nome_arquivo):
+    strings = []
+    try:
+        with open(nome_arquivo, 'r') as arquivo:
+            for linha in arquivo:
+                strings.append(linha.strip())
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+
+    return strings
+
+nome_arquivo = "output.txt"
+
+strings = let_arquivo_txt(nome_arquivo)
+
+imprimir_string(strings)
