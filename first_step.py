f1728af507eed1f8bfb018cf10d38be6ed7a4204
Leticia-SS <leticia.saraiva013@gmail.com>
2025-08-24 22:22:45 -0300

    Bubble sort method

--- first_step.py
+++ first_step.py
@@ -26,6 +26,34 @@ def escrever_lista_em_arquivo(lista, nome_arquivo):
             arquivo.write(str(item) + "\n")
 
 
+def bubble_sort(lista):
+    arr = lista.copy()
+    n = len(arr)
+
+    for i in range(n):
+        trocou = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                trocou = True
+
+        if not trocou:
+            break
+
+    return arr
+
+inicio_bubble = time.time()
+resultado_bubble = bubble_sort(lista)
+fim_bubble = time.time()
+total_bubble = fim_bubble - inicio_bubble
+
+escrever_lista_em_arquivo(resultado_bubble, "bubble_sorted.txt")
+
+print("\n=============================")
+print("Bubble Sort")
+print(f"Tempo de execução: {total_bubble} segundos")
+print(f"Total de elementos ordenados: {len(resultado_bubble)}")
+
 
 
 
